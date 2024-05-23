d4c18e49f8cae993c7ab2be575ce08598f48f229
jashonmichael <jashon.michael@student.moringaschool.com>
2024-05-23 11:42:41 +0300

    Complete lab

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The Person is walking")
+
+Michael = Person()
+Michael.talk()
+
+Michael = Person()
+Michael.walk()
